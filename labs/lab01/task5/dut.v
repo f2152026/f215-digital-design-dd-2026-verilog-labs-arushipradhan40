aa8d922e116d7dcba61e1581347404312e45f72c
arushipradhan40 <f20240834@pilani.bits-pilani.ac.in>
2026-08-29 06:41:32 +0000

    lab01 task1

--- labs/lab01/task5/dut.v
+++ labs/lab01/task5/dut.v
@@ -20,6 +20,5 @@ module dut(
   // rca64 U_IMPL (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
   // cla64_flat U_IMPL (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
   // cla64_blocked U_IMPL (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
-  
 
 endmodule
